2d2f1e05066b4be398718f8ae9c12334ba1ff934
LLS-lopos <luderickls@gmail.com>
2024-04-18 19:29:13 -0400

    fichier blend modifier + numero de version

--- SplitScreen/splitscreen.py
+++ SplitScreen/splitscreen.py
@@ -2,32 +2,49 @@
 # Python Component for Splitscreen
 # script by Ludérïck Le Saouter @LLS
 # CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0/)
-# creation: 16/04/2024
+# update V2: 18/04/2024
 
 import bge
 from collections import OrderedDict
 
 class Splitscreen(bge.types.KX_PythonComponent):
     args = OrderedDict([
+        ("Activate", False),
 		("Camera", ""),
 		("Left", 0.0),
         ("Bottom", 0.0),
 		("Right", 1.0),
-		("Top", 1.0)
+		("Top", 1.0),
+        ("PropertyActif", "")
 	])
     
     def start(self, args):
+        self.actif = args["Activate"]
         self.gauche = args["Left"]
         self.bas = args["Bottom"]
         self.droite = args["Right"]
         self.haut = args["Top"]
         self.camera = args["Camera"]
+        self.actionneur = args["PropertyActif"]
+        
+        self.scene = bge.logic.getCurrentScene()
+        self.objet = self.scene.objects
+        self.cam = self.scene.objects[self.camera]
     
+    def activation(self):
+        prop = str(self.actionneur)
+        prop_verifier = [obj for obj in self.objet if prop in obj]
+        for element in prop_verifier:
+            if element[prop] == True:
+                self.actif = True
+                break
+            else:
+                self.actif = False
+        
     def tailleEcran(self):
         self.ecran_H = bge.render.getWindowHeight()
         self.ecran_L = bge.render.getWindowWidth()
         self.scene = bge.logic.getCurrentScene()
-        self.cam = self.scene.objects[self.camera]
         
         Gauche = self.ecran_L*self.gauche; Bas = self.ecran_H*self.bas; Droite = self.ecran_L*self.droite; Haut = self.ecran_H*self.haut
         
@@ -40,4 +57,11 @@ class Splitscreen(bge.types.KX_PythonComponent):
         self.cam.useViewport = True
     
     def update(self):
-        self.tailleEcran()
+        self.activation()
+        if self.actionneur is not None:
+            if self.actif is True:
+                self.tailleEcran()
+            else:
+                self.cam.useViewport = False
+        else:
+            self.tailleEcran()
